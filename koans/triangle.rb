d5ae9861f1c70e71acbaef1c458bb4d8ce0ca5fb
NUTTENS Florent <florent.nuttens@e-i.com>
2020-11-12 13:41:19 +0100

    solve about_triangle_project_2 koan

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
+  end
+
+  if a + b <= c || b + c <= a || a + c <= b
+    raise TriangleError
+  end
+
   if a == b && b == c
     return :equilateral
   end
